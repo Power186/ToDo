f12d061b4113fa133495e9981469b55fab14e6d5
Scott <agentwinburn1@gmail.com>
2021-01-07 14:22:31 +0800

    model, UI

---
+++ ToDoList/ToDo.swift
@@ -0,0 +1,33 @@
+//
+//  ToDo.swift
+//  ToDoList
+//
+//  Created by Scott on 1/7/21.
+//
+
+import UIKit
+
+struct ToDo: Equatable {
+    let id = UUID()
+    var title: String
+    var image: UIImage?
+    var isComplete: Bool
+    var dueDate: Date
+    var notes: String?
+    
+    static func ==(lhs: ToDo, rhs: ToDo) -> Bool {
+        return lhs.id == rhs.id
+    }
+    
+    static func loadToDos() -> [ToDo]? {
+        return nil
+    }
+    
+    static func loadSampleToDos() -> [ToDo] {
+        let todo1 = ToDo(title: "ToDo 1", image: nil, isComplete: false, dueDate: Date(), notes: "Notes 1")
+        let todo2 = ToDo(title: "ToDo 2", image: nil, isComplete: true, dueDate: Date(), notes: "Notes 2")
+        let todo3 = ToDo(title: "ToDo 3", image: nil, isComplete: false, dueDate: Date(), notes: "Notes 3")
+        
+        return [todo1, todo2, todo3]
+    }
+}
